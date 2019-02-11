54e86d9c174a11cff613462cc425c3ce9aa99530
Debabrata <Debabrata@LAPTOP-6G0J1SS8>
2019-02-12 02:18:14 +0530

    3rd commit

--- RestWebService/src/com/debu/test/dao/TestDao.java
+++ RestWebService/src/com/debu/test/dao/TestDao.java
@@ -27,8 +27,11 @@ public static String eliminate(String input) {
 		int leng=charstr.length;
 		for (int i = 0; i < leng; i++) {
 		int currentAsciicode=Character.getNumericValue(charstr[i]);
+		//assign previous character only if the current character is not the first characted, else set to 0
 		int previousAsciicode=i!=0?Character.getNumericValue(charstr[i-1]):0;
+		//assign next character only if the current character is not the last characted, else set to 0
 		int nextasciicode=i!=leng-1?Character.getNumericValue(charstr[i+1]):0;
+		//if the current character is in the middle, check sequence with both previous and next character
 		if((currentAsciicode==previousAsciicode+1)||(currentAsciicode==nextasciicode-1)){
 			continue;}
 		output+=charstr[i];
